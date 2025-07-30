4b37802f0c310482ca476b5071c44b31fc34329d
bruno <bruno.maffre@maides.one>
2025-07-30 08:39:36 +0400

    fix: debug et correction des pad_token

--- opco_4_utils/utils.py
+++ opco_4_utils/utils.py
@@ -1,19 +1,30 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
+from typing import Optional
 tokenizer = None
 initialized = False
 
-def init_model(model_name, device=None):
+def init_model(model_name, device=None, pad_token=None):
     """
     Initialise le tokenizer et le model identifié par 'model_name'
     :param model_name:
     :return:
     """
+    global tokenizer, initialized
     # Création du tokenniser
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     # Chargement du model
     model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    # Definition du pad_token si pas fourni
+    if pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        model.config.pad_token_id = tokenizer.pad_token_id
+        print("Tokenizer par defaut")
+    else:
+        tokenizer.pad_token = pad_token
+        print("Tokenizer custom")
+
 
     if device is not None:
         model.to(device)
@@ -28,11 +39,16 @@ def do_predict(model,
                temperature: float = 0.7,
                top_k: int = 40,
                top_p: float = 0.9,
-               pad_token_id: int = 0):
+               pad_token: Optional[int] = None):
 #               pad_token_id: int = tokenizer.eos_token_id):
 
     if not initialized:
         raise RuntimeError('You need to call init_model first')
+
+    if pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+    else:
+        tokenizer.pad_token = pad_token
 
     print(f"Prompte : {prompt}\n----")
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
@@ -43,7 +59,7 @@ def do_predict(model,
                                    temperature=temperature,
                                    top_k=top_k,
                                    top_p=top_p,
-                                   pad_token_id=pad_token_id,
+                                   pad_token_id=pad_token,
                                    do_sample=True)
     print("----")
     print(generated_ids[0])
